20bad58a63d335112ce97b0fec015fd0bdbb38a8
Tarichikato <pierrelourdelet@gmail.com>
2020-03-27 12:30:18 +0100

    shitCode

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
-//const TodoList = artifacts.require("./TodoList.sol");
-const Pierre = artifacts.require("./PierreDS.sol");
-const Rudy = artifacts.require("./RudyDS.sol");
+ 
+const DiplomaStorage = artifacts.require("./DiplomaStorage.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(TodoList);
-  deployer.deploy(Pierre);
-  deployer.deploy(Rudy);
+  deployer.deploy(DiplomaStorage);
 };
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+ 
 App = {
   loading: false,
   contracts: {},
@@ -43,98 +44,99 @@ App = {
   },
 
   loadAccount: async () => {
-    // Set the current blockchain account
     App.account = web3.eth.accounts[0]
+    console.log(App.account)
   },
 
   loadContract: async () => {
-    // Create a JavaScript version of the smart contract
-    const todoList = await $.getJSON('TodoList.json')
-    App.contracts.TodoList = TruffleContract(todoList)
-    App.contracts.TodoList.setProvider(App.web3Provider)
+    const diplomaStorage = await $.getJSON('DiplomaStorage.json')
+    App.contracts.DiplomaStorage = TruffleContract(diplomaStorage)
+    App.contracts.DiplomaStorage.setProvider(App.web3Provider)
+    console.log(diplomaStorage)
 
-    // Hydrate the smart contract with values from the blockchain
-    App.todoList = await App.contracts.TodoList.deployed()
+    App.diplomaStorage = await App.contracts.DiplomaStorage.deployed()
   },
 
   render: async () => {
-    // Prevent double render
     if (App.loading) {
       return
     }
 
-    // Update app loading state
+    //Update app loading taskTemplate
     App.setLoading(true)
 
     // Render Account
     $('#account').html(App.account)
 
     // Render Tasks
-    await App.renderTasks()
+    await App.renderStudents()
 
     // Update loading state
     App.setLoading(false)
   },
 
-  renderTasks: async () => {
-    // Load the total task count from the blockchain
-    const taskCount = await App.todoList.taskCount()
-    const $taskTemplate = $('.taskTemplate')
+  renderStudents: async () => {
+      // Load the total task count from the blockchain
+      const studentCount = await App.diplomaStorage.studentCount()
+      const $taskTemplate = $('.taskTemplate')
 
-    // Render out each task with a new task template
-    for (var i = 1; i <= taskCount; i++) {
-      // Fetch the task data from the blockchain
-      const task = await App.todoList.tasks(i)
-      const taskId = task[0].toNumber()
-      const taskContent = task[1]
-      const taskCompleted = task[2]
+      // Render out each task with a new task template
+      for (var i = 1; i <= studentCount; i++) {
+        // Fetch the task data from the blockchain
+        const student = await App.diplomaStorage.students(i)
+        const idStudent = student[0].toNumber()
+        const studentFirstName = student[1]
+        const studentLastName = student[2]
+        const studentDay = student[3].toNumber()
+        const studentMonth = student[4].toNumber()
+        const studentYear = student[5].toNumber()
 
-      // Create the html for the task
-      const $newTaskTemplate = $taskTemplate.clone()
-      $newTaskTemplate.find('.content').html(taskContent)
-      $newTaskTemplate.find('input')
-                      .prop('name', taskId)
-                      .prop('checked', taskCompleted)
-                      .on('click', App.toggleCompleted)
+        // Create the html for the task
+        const $newTaskTemplate = $taskTemplate.clone()
+        $newTaskTemplate.find('.studentFirstName').html(studentFirstName)
+        $newTaskTemplate.find('input')
+                        .prop('name', idStudent)
+                        // .prop('checked', taskCompleted)
+                        // .on('click', App.toggleCompleted)
 
-      // Put the task in the correct list
-      if (taskCompleted) {
-        $('#completedTaskList').append($newTaskTemplate)
-      } else {
-        $('#taskList').append($newTaskTemplate)
+        // Put the task in the correct list
+        // if (taskCompleted) {
+        //   $('#completedTaskList').append($newTaskTemplate)
+        // } else {
+        $('#studentList').append($newTaskTemplate)
+        // }
+
+        // Show the task
+        $newTaskTemplate.show()
       }
+    },
 
-      // Show the task
-      $newTaskTemplate.show()
-    }
-  },
-
-  createTask: async () => {
+  createStudent: async () => {
     App.setLoading(true)
-    const content = $('#newTask').val()
-    await App.todoList.createTask(content)
+    const studentFirstName = $('#newStudent').val()
+    await App.diplomaStorage.createStudent(studentFirstName)
     window.location.reload()
   },
 
-  toggleCompleted: async (e) => {
-    App.setLoading(true)
-    const taskId = e.target.name
-    await App.todoList.toggleCompleted(taskId)
-    window.location.reload()
-  },
+  //   toggleCompleted: async (e) => {
+  //   App.setLoading(true)
+  //   const taskId = e.target.name
+  //   await App.todoList.toggleCompleted(taskId)
+  //   window.location.reload()
+  // },
 
   setLoading: (boolean) => {
-    App.loading = boolean
-    const loader = $('#loader')
-    const content = $('#content')
-    if (boolean) {
-      loader.show()
-      content.hide()
-    } else {
-      loader.hide()
-      content.show()
-    }
-  }
+   App.loading = boolean
+   const loader = $('#loader')
+   const studentFirstName = $('#studentFirstName')
+   if (boolean) {
+     loader.show()
+     studentFirstName.hide()
+   } else {
+     loader.hide()
+     studentFirstName.show()
+   }
+ }
 }
 
 $(() => {
