288c72eb2dff58e684fb46094dd061159fdb167d
karkat413 <56168177+karkat413@users.noreply.github.com>
2020-05-07 10:30:02 +0200

    Delete DiplomaStorageContract.js

--- src/ethereum/DiplomaStorageContract.js
+++
@@ -1,7 +0,0 @@
-import { web3 } from "./web3"
-import DiplomaStorageABI from "./DiplomaStorageABI"
-
-export function createContract(contractAddress) {
-    return new web3.eth.Contract(DiplomaStorageABI, '0xbE41F8E5073E899aa266De4f6603cec84Bb073bc')
-    
-}
