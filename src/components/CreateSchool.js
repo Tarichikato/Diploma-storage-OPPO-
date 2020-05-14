186c4c83575191629c1d215b63b4cb087c422624
raphael33160 <57766316+raphael33160@users.noreply.github.com>
2020-05-14 11:29:21 +0200

    Update CreateSchool.js

--- src/components/CreateSchool.js
+++ src/components/CreateSchool.js
@@ -1,168 +1,188 @@
 import React, { Component }  from 'react'
-import { createContract } from '../ethereum/DiplomaStorageContract'
-import { Table, Button,Form,Checkbox } from 'semantic-ui-react'
-import Web3 from 'web3'
-import { web3 } from '../ethereum/web3'
+import { createContract } from './../ethereum/DiplomaStorageContract'
+import { Button, ButtonGroup, Form, Spinner, Modal, ButtonToolbar } from 'react-bootstrap';
+import { web3 } from './../ethereum/web3';
+import NavBar from './../assets/NavBar';
+import { PopupSchool } from './../assets/PopupSchool';
+
 
 
 
 export class CreateSchool extends Component {
 
-    state = {
-       
+
+      state = {
+          schools: {
             idSchool: 0,
             address: "",
             schoolName: 'N/A',
             schoolCount: 0,
             answer: '',
-        
+          }
 
+      }
+
+    async componentDidMount () {
+      await this.getSchools(this.getDiplomaStorageAddress()
+      )
     }
 
-  async componentDidMount () {
-    this.loadBcData()
-    const contract = createContract(this.getDiplomaStorageAddress())
-    this.setState({contract})
-    console.log("contrat",contract)
-    this.getContractState(contract)
+    getDiplomaStorageAddress () {
+      return this.props.match.params.address
+    }
+
+    async getSchools(address) {
+      const contract = createContract(address)
+      const accounts = await web3.eth.getAccounts()
+      this.setState({ account: accounts[0] })
+      console.log(accounts)
+
+      this.setState({ contract })
+      console.log(contract)
+
+      const schoolCount = await contract.methods.schoolCount().call()
+      this.setState({schoolCount})
+
+      for (var i = 1; i <= schoolCount; i++) {
+          const school = await contract.methods.schools(i).call()
+          this.setState({
+            schools: [...this.state.schools, school]
+          })
+        }
+
   }
 
-
-  async getContractState(contract){
-    this.setState({loading:true})
-    const master = contract.methods.master().call()
-    console.log('master')
-    const studentCount = await contract.methods.studentCount().call()
-    this.setState({studentCount: studentCount})
-    console.log('studentCount GCS',studentCount)
-  }
-
-  async loadBcData(){
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const network = await web3.eth.net.getNetworkType()
-    console.log("network",network)
-    const accounts = await web3.eth.getAccounts()
-    console.log("account", accounts[0])
-    this.setState({account: accounts[0]})
-    console.log("addresseContrat",this.getDiplomaStorageAddress())
-
-    const contract = createContract(this.getDiplomaStorageAddress())
-    this.setState({contract})
-  }
- 
   constructor(props) {
-    super(props)
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.state = {
-      
-      idSchool: 0,
-      address: "",
-      schoolName: 'N/A',   
-      schoolCount: 0,
-      answer: '',
+      super(props)
+      this.state = {
+        account:'',
+        schoolCount: 0,
+        schools: [],
+        ModalShow: false,
+      }
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+  }
+
+
+  createSchool(schoolName,address) {
+      this.state.contract.methods.createSchool(schoolName,address).send({ from: this.state.account })
+      console.log("Account" , this.state.account)
+  }
+
+
+      onChange(event) {
+
+      const target = event.target;
+      const value =  target.value;
+      const name = target.name;
+      console.log(name,value,this.state)
+      this.setState({
+        [name]: value
+      });
+      console.log(name,value,this.state)
+  }
+
+
+      onSubmit(event) {
+      event.preventDefault();
+      this.createSchool(this.state.schoolName,this.state.address)
     }
-  }
-  
-  
-  getDiplomaStorageAddress () {
-    return '0xfd4420CA2Da4060147AbA6A4bB49A9494a38E33b'
-  }
 
 
-  
+    render() {
+      let ModalClose =() => this.setState({ModalShow:false})
 
-
-   async createSchool(address,schoolName) {
-    this.setState({loading:true})
-    const contract = createContract(this.getDiplomaStorageAddress())
-    this.setState({contract})
-    const master = await contract.methods.master().call()
-    console.log('master',master)
-    const answer = await contract.methods.createSchool(address,schoolName).call()
-    const schoolCount = await contract.methods.schoolCount().call()
-    this.setState({answer : answer})
-    console.log('answer',answer)
-    this.setState({schoolCount: schoolCount})
-    console.log("NEW schoolCount",schoolCount)
-  } 
-
-  async onChange(event) {
-    
-    const target = event.target;
-    const value =  target.value;
-    const name = target.name;
-    console.log('OC1',name,value,this.state) 
-    await this.setState({
-      [name]: value
-    });
-    console.log('OC2',name,value,this.state)  
-}
-
-
-
-
-
-  async onSubmit(event) {
-  event.preventDefault();
-  await this.createSchool(this.state.address,this.state.schoolName)
-}
-
- 
-
-          render() {
-    return (
-
-      
-      
+      return (
         <div>
+          <div>
+              <NavBar/>
+          </div>
 
-<p>Account : {this.state.account}</p>
-
-<p>SchoolCount : {this.state.schoolCount}</p>
-
-<p>Contract address: {this.getDiplomaStorageAddress()}</p>
-
-
-<form>
-        <label>
-          adresse :
-          <input
-            placeholder= 'Enter adresse here'
-            name="address"
-            type="text"
-            //value={this.state.INE}
-            onChange={this.onChange} />
-        </label>
-        <br />
-        <label>
-          schoolName :
-          <input
-            placeholder='School Name'
-            name="schoolName"
-            type="text"
-            //value={this.state.firstName}
-            onChange={this.onChange} />
-        </label>
-        
-      </form>
-
-      <Button
-          type='submit'
-          onClick={this.onSubmit}
-          >
-          Submit
-      </Button>
-
-          
-        
-
-        <p>adress: {this.state.adress}</p>
-        <p>schoolName: {this.state.schoolName}</p>
-        
+          <div className="container">
+            <div className="row mt-5 text-center center">
+              <div className="col-lg-10 mb-4 grid-margin">
+                <div className="card h-100">
+                    <h4 className="card-header">Create School</h4>
+                    <div className="card-body">
+                      <p className="card-text">Rentrez les informations de l école que vous voulez créer:  </p>
+                    </div>
+                </div>
+              </div>
+          </div>
         </div>
-    );
-  }
+
+
+          <Form>
+              <Form.Group controlId="formGroupEmail">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                      placeholder= 'Enter the address'
+                      name="address"
+                      type="text"
+                      onChange={this.onChange} />
+              </Form.Group>
+
+              <Form.Group controlId="formGroupPassword">
+                  <Form.Label>SchoolName</Form.Label>
+                  <Form.Control
+                      placeholder='Enter the schoolName'
+                      name="schoolName"
+                      type="text"
+                      onChange={this.onChange} />
+              </Form.Group>
+
+
+          </Form>
+
+          <div>
+
+        <ButtonToolbar>
+          <Button variant="primary"
+            onClick={() => this.setState({ModalShow: true})}
+            >Create School
+          </Button>
+
+          <PopupSchool
+            show={this.state.ModalShow}
+            onHide={ModalClose}
+            onSubmit={this.onSubmit}
+          />
+        </ButtonToolbar>
+
+          </div>
+
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col-lg-4 mb-4 grid-margin">
+            <ButtonGroup size="lg" >
+                  <Button variant="primary"
+                      onClick={this.onSubmitBack}
+                      >Back
+                  </Button>
+                  <Button variant="light"
+                      onClick={this.onSubmitReload}
+                      >Refresh
+                  </Button>
+            </ButtonGroup>
+          </div>
+        </div>
+      </div>
+
+        </div>
+      );
+    }
+
+    onSubmitBack(event) {
+      event.preventDefault();
+      window.history.back()
+      }
+
+    onSubmitReload(event) {
+      event.preventDefault();
+      window.location.reload()
+      }
+
 }
 
 export default CreateSchool;
